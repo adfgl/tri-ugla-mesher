97f84ee1b05d1ef55bcf874604c10f19f47c0438
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-04 19:30:37 +0200

    Update Face.cs

--- TriUglaMesher/TriUgla.Mesher/MeshCore/Face.cs
+++ TriUglaMesher/TriUgla.Mesher/MeshCore/Face.cs
@@ -1,8 +1,15 @@
-﻿namespace TriUgla.Mesher.MeshCore
+namespace TriUgla.Mesher.MeshCore
 {
     public sealed class Face : MeshElement
     {
-        public Edge Edge { get; set; } = null!;
-        public int Depth { get; set; } = -1;
+        public Edge Edge { get; internal set; } = null!;
+        public int Depth { get; internal set; } = -1;
+        public FaceKind Kind 
+        {
+            get
+            {
+
+            }
+        }
     }
 }
