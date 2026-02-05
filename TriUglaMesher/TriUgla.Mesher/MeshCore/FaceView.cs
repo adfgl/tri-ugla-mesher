07bb9c30f1dead613983d61b541ade8f3b234377
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-05 09:30:19 +0200

    Update FaceView.cs

--- TriUglaMesher/TriUgla.Mesher/MeshCore/FaceView.cs
+++ TriUglaMesher/TriUgla.Mesher/MeshCore/FaceView.cs
@@ -1,4 +1,9 @@
-public readonly struct FaceView
+public readonly struct FaceView(Mesh mesh, FaceId id)
 {
+    public readonly FaceId id = id;
+    readonly Mesh _m = mesh;
 
+    ref Face Self() => ref _m.Faces.Ref(id);
+
+    public EdgeView Edge() => new EdgeView(_m, Self().edge);
 }
