3f2c55f5a75e4653879f5af282ebacce12a3e2f2
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-05 09:04:05 +0200

    Update EdgeView.cs

--- TriUglaMesher/TriUgla.Mesher/MeshCore/EdgeView.cs
+++ TriUglaMesher/TriUgla.Mesher/MeshCore/EdgeView.cs
@@ -1,4 +1,14 @@
-public readonly struct EdgeView
+public readonly struct EdgeView(Mesh mesh, Id id)
 {
+    public readonly Id id = id;
+    readonly Mesh _m = mesh;
 
+    ref Edge Self() => ref _m.Edges.Ref(id);
+
+    public EdgeView Next() => new EdgeView(_m, Self().next);
+    public EdgeView Prev() => new EdgeView(_m, Self().prev);
+    public EdgeView Twin() => new EdgeView(_m, Self().twin);
+    public NodeView NodeStart() => new NodeView(_m, Self().nodeStart);
+    public NodeView NodeEnd() => new NodeView(_m, Self().nodeEnd);
+    public FaceView Face() => new FaceView(_m, Self().face);
 }
