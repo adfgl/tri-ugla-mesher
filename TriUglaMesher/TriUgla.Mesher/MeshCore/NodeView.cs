705597445f9e72600cf3affd2ad8ec439d3d24b0
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-05 09:31:58 +0200

    Update NodeView.cs

--- TriUglaMesher/TriUgla.Mesher/MeshCore/NodeView.cs
+++ TriUglaMesher/TriUgla.Mesher/MeshCore/NodeView.cs
@@ -1,4 +1,10 @@
-public readonly struct NodeView
+public readonly struct NodeView(Mesh mesh, NodeId id)
 {
+    public readonly NodeId id = id;
 
+    readonly Mesh _m = mesh;
+
+    ref Node Self() => ref _m.Nodes.Ref(id);
+
+    public EdgeView Edge() => new EdgeView(_m, Self().edge);
 }
