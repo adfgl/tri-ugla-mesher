31a5c35852aa442624329d14c317844f0df024de
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-06 13:36:17 +0200

    Update Mesh.cs

--- TriUglaMesher/TriUgla.Mesher/Mesh.cs
+++ TriUglaMesher/TriUgla.Mesher/Mesh.cs
@@ -24,13 +24,72 @@ namespace TriUgla.Mesher
 
             
         }
+
+        public void Invalidate(Face face)
+        {
+            Edge e0 = face.Edge;
+            Edge e = e0;
+            do
+            {
+                e.Invalid = true;
+                e = e.Next;
+            } while (!ReferenceEquals(e0, e))
+        }
+
+        public bool TryReconnect(Node node)
+        {
+            Edge e0 = node.Edge;
+            Edge e = e0;
+            do
+            {
+                if (!e.Invalid)
+                {
+                    node.Edge = e;
+                    return true;
+                }
+                e = e.Prev.Twin!;
+            } while (!ReferenceEquals(e0, e))
+
+            return false;
+        }
+    }
+
+    public class ElementRemover
+    {
+
+    }
+
+    public class ElementInvalidator
+    {
+        public void Invalidate(Node node)
+        {
+            if (node.Invalid) return;
+            node.Invalid = true;
+        }
+
+        public void Invalidate(Edge edge)
+        {
+            if (edge.Invalid) return;
+            edge.Invalid = true;
+        }
+
+        public void Invalidate(Face face)
+        {
+            Edge e0 = face.Edge;
+            Edge e = e0;
+            do
+            {
+                Invalidate(e);
+                e = e.Next;
+            } while (!ReferenceEquals(e0, e))
+        }
     }
 
     public class MeshElement
     {
         int _visitStamp = 0;
 
-        public bool Dead { get; internal set; }
+        public bool Invalid { get; internal set; }
         public int Payload { get; set; }
 
         public bool TryVisit(int stamp)
