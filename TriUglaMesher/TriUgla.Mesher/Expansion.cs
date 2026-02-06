59d22de7f8ad7d790a5b8014a4c297d6f58d2794
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-06 09:39:09 +0200

    Update Expansion.cs

--- TriUglaMesher/TriUgla.Mesher/Expansion.cs
+++ TriUglaMesher/TriUgla.Mesher/Expansion.cs
@@ -1,4 +1,13 @@
 public class Expansion
 {
-
+    public void Sum(double a, double b, 
+            out double stored,
+            out double lost)
+    {
+        stored = a + b;
+        double storedB = stored - a;
+        double lostA = stored - b;
+        double lostB = b - bStored;
+        lost = lostA + lostB;
+    }
 }
