e89aa3eae8f111db0ce1a7db8e8424cb55c59b8b
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-06 12:00:05 +0200

    Update Mesh.cs

--- TriUglaMesher/TriUgla.Mesher/Mesh.cs
+++ TriUglaMesher/TriUgla.Mesher/Mesh.cs
@@ -5,15 +5,31 @@ namespace TriUgla.Mesher
 {
     public sealed class Mesh
     {
+        public Face Root { get; set; }
         
-        
+        public bool Remove(Face face)
+        {
+            if (face.Dead) 
+            {
+                return false;
+            }
+
+            Edge e0 = face.Edge;
+            Edge e = e0;
+            do 
+            {
+                e.Dead = true;
+                e = e.Next;
+            } while (!ReferenceEquals(e0, e))
+
+            
+        }
     }
 
     public class MeshElement
     {
         int _visitStamp = 0;
 
-        public int Id { get; internal set; }
         public bool Dead { get; internal set; }
         public int Payload { get; set; }
 
