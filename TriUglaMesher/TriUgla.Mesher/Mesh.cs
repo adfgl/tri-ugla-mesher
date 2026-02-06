19dc24d6738c993058aa9cd4a37aa230fd261f32
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-06 11:19:50 +0200

    Update Mesh.cs

--- TriUglaMesher/TriUgla.Mesher/Mesh.cs
+++ TriUglaMesher/TriUgla.Mesher/Mesh.cs
@@ -1,20 +1,29 @@
-﻿using TriUgla.Mesher.MeshCore;
+using TriUgla.Mesher.MeshCore;
 using TriUgla.Mesher.Utils;
 
 namespace TriUgla.Mesher
 {
-    public sealed class Mesh(int capacity = 16)
+    public sealed class Mesh
     {
-        public Store<Node> Nodes = new Store<Node>(capacity * 2 + 1);
-        public Store<Edge> Edges = new Store<Edge>(capacity * 2 + 1);
-        public Store<Face> Faces = new Store<Face>(capacity);
+        
+        
+    }
 
-        public ref Node Ref(NodeId id) => ref Nodes.Ref(id.value);
-        public ref Edge Ref(EdgeId id) => ref Edges.Ref(id.value);
-        public ref Face Ref(FaceId id) => ref Faces.Ref(id.value);
+    public sealed class MeshElement
+    {
+        int _visitStamp = 0;
 
-        public NodeView Node(NodeId id) => new NodeView(this, id);
-        public EdgeView Edge(EdgeId id) => new EdgeView(this, id);
-        public FaceView Face(FaceId id) => new FaceView(this, id);
+        public int Id { get; internal set; }
+        public bool Dead { get; internal set; }
+        
+        public bool TryVisit(int stamp)
+        {
+            if (_visitsStamp != stamp)
+            {
+                _visitsStamp = stamp;
+                return true;
+            }
+            return false;
+        }
     }
 }
