af63abdb497d02f475776a9e326c2c483419124d
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-06 14:31:17 +0200

    Update Mesh.cs

--- TriUglaMesher/TriUgla.Mesher/Mesh.cs
+++ TriUglaMesher/TriUgla.Mesher/Mesh.cs
@@ -14,29 +14,126 @@ namespace TriUgla.Mesher
                 return false;
             }
 
-            Edge e0 = face.Edge;
-            Edge e = e0;
-            do 
-            {
-                e.Dead = true;
-                e = e.Next;
-            } while (!ReferenceEquals(e0, e))
+            Invalidate
 
             
         }
 
+        
+    }
+
+    public class ElementRemover
+    {
+        public void Remove(Node node)
+        {
+            Edge? edge = node.Edge;
+            if (esge is null) return;
+
+            node.Edge = null;
+            if (ReferenceEquals(edge.Start, node))
+            {
+                edge.Start = null;
+            }
+        }
+
+        public void Remove(Edge edge)
+        {
+            if (edge.Start is not null)
+            {
+                Remove(edge.Start);
+            }
+            
+            Face? face = edge.Face;
+            if (face is not null)
+            {
+                if (ReferenceEquals(face.Edge, edge))
+                {
+                    face.Edge = null;
+                }
+                edge.Face = null;
+            }
+
+            Edge? twin = edge.Twin;
+            if (twin is not null)
+            {
+                twin.Twin = null;
+                edge.Twin = null;
+            }
+
+            Edge? next = edge.Next;
+            if (next is not null)
+            {
+                next.Prev = null;
+                edge.Next = null;
+            }
+
+            Edge? prev = edge.Prev;
+            if (prev is not null)
+            {
+                prev.Next = null;
+                edge.Prev = null;
+            }
+        }
+
+        void DetatchPrev(Edge? prev)
+        {
+            if (prev is null) return;
+            prev.Next = null;
+            edge.Prev = null;
+        }
+
+        void DetatchNext(Edge? next)
+        {
+            if (next is null) return;
+            next.Prev = null;
+            edge.Prev = null;
+        }
+    }
+
+    public class ElementInvalidator
+    {
+        public void Invalidate(Node node)
+        {
+            node.Invalid = true;
+        }
+
+        public void Invalidate(Edge edge)
+        {
+            edge.Invalid = true;
+        }
+
         public void Invalidate(Face face)
+        {
+            if (face.Invalid) return;
+            InvalidateEdges(face);
+
+            Edge e0 = face.Edge;
+            Edge e = e0;
+            do
+            {
+                Node n = e.Start;
+                if (!TryReconnect(n))
+                {
+                    Invalidate(n);
+                }
+                e = e.Next;
+            } while (!ReferenceEquals(e0, e))
+
+            face.Invalid = true;
+        }
+
+        void InvalidateEdges(Face face)
         {
             Edge e0 = face.Edge;
             Edge e = e0;
             do
             {
-                e.Invalid = true;
+                Invalidate(e);
                 e = e.Next;
             } while (!ReferenceEquals(e0, e))
         }
 
-        public bool TryReconnect(Node node)
+        bool TryReconnect(Node node)
         {
             Edge e0 = node.Edge;
             Edge e = e0;
@@ -51,37 +148,6 @@ namespace TriUgla.Mesher
             } while (!ReferenceEquals(e0, e))
 
             return false;
-        }
-    }
-
-    public class ElementRemover
-    {
-
-    }
-
-    public class ElementInvalidator
-    {
-        public void Invalidate(Node node)
-        {
-            if (node.Invalid) return;
-            node.Invalid = true;
-        }
-
-        public void Invalidate(Edge edge)
-        {
-            if (edge.Invalid) return;
-            edge.Invalid = true;
-        }
-
-        public void Invalidate(Face face)
-        {
-            Edge e0 = face.Edge;
-            Edge e = e0;
-            do
-            {
-                Invalidate(e);
-                e = e.Next;
-            } while (!ReferenceEquals(e0, e))
         }
     }
 
