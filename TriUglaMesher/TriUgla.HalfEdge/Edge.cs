5ae8e11186c4137a8ea628c08f4273f3ef66be64
adfgl <69821550+adfgl@users.noreply.github.com>
2026-02-18 11:41:42 +0200

    Update Edge.cs

--- TriUglaMesher/TriUgla.HalfEdge/Edge.cs
+++ TriUglaMesher/TriUgla.HalfEdge/Edge.cs
@@ -1,4 +1,4 @@
-﻿using TriUgla.Geometry;
+using TriUgla.Geometry;
 
 namespace TriUgla.HalfEdge
 {
@@ -109,6 +109,37 @@ namespace TriUgla.HalfEdge
                 EdgeKind.Hole => Release(ref _holes),
                 _ => throw new NotImplementedException(nameof(kind)),
             };
+        }
+
+        public static Edge? FindDirected(Node a, Node b)
+        {
+            Edge? edge = null;
+
+            Edge e0 = a.Edge;
+            Edge e = e0;
+            do
+            {
+                if (e.NodeEnd == b)
+                {
+                    edge = e;
+                    break;
+                }
+                Edge? next = e.Prev.Twin;
+                if (next is null)
+                {
+                    break;
+                }
+                e = next;
+            } while (e0 != e);
+
+            return edge;
+        }
+
+        public static Edge? Find(Node a, Node b)
+        {
+            Edge? edge = FindDirected(a, b);
+            edge ??= FindDirected(b, a);
+            return edge;
         }
 
         void Constrain(ref int count)
